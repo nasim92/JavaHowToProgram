4cbe97da93c4c8e554e49b26128317dec2d68291
NasimSalmani <nasimsalmani172@yahoo.com>
2016-02-18 23:29:26 +0330

    Chapter 6 SecureRandomForm

--- src/main/java/JavaHowToProgram/ch6/Main.java
+++ src/main/java/JavaHowToProgram/ch6/Main.java
@@ -6,6 +6,8 @@ package main.java.JavaHowToProgram.ch6;
 public class Main {
     public static void main(String args[]) {
 
-        MaximumFinderForm mnf = new MaximumFinderForm();
+//        MaximumFinderForm mnf = new MaximumFinderForm();
+
+        SecureRandomForm app = new SecureRandomForm();
     }
 }
---
+++ src/main/java/JavaHowToProgram/ch6/SecureRandomForm.java
@@ -0,0 +1,56 @@
+package main.java.JavaHowToProgram.ch6;
+
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.security.SecureRandom;
+
+public class SecureRandomForm extends JFrame{
+    private JPanel mainPanel;
+    private JTextArea textArea;
+    private JButton generateRandomButton;
+
+
+    public SecureRandomForm(){
+        mainPanel = new JPanel(new FlowLayout());
+        textArea = new JTextArea();
+        generateRandomButton = new JButton("generateRandomButton");
+
+        mainPanel.add(textArea);
+        mainPanel.add(generateRandomButton);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().add(mainPanel);
+
+        setVisible(true);
+        setSize(500, 500);
+
+        pack();
+        generateRandomButton.addActionListener(new GenerateRandomButtonActionListener());
+    }
+
+    public void RandomIntegers() {
+        SecureRandom randomNumbers = new SecureRandom();
+
+        for (int counter = 1; counter <= 20; counter++) {
+            int face = 1 + randomNumbers.nextInt(6);
+
+            System.out.printf("%d ", face);
+
+            if (counter % 5 == 0)
+                System.out.println();
+        }
+    }
+
+
+
+    public class GenerateRandomButtonActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+         
+        }
+    }
+}
